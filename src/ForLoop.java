2d0abeccc51fba7a7903e8897d89da36fc5f1454
bartosz.lagoda <composit095@gmail.com>
2022-06-09 22:26:48 +0200

    Commit ForLoop.java

---
+++ src/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 0; i<100; i = i + 2){
+            System.out.println(i);
+        }
+        for(int j = 0; j<100; j++){
+            if(j%5==0){
+                System.out.println(j);
+            }
+        }
+    }
+
+
+}
