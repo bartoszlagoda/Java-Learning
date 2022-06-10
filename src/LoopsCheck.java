d087322d1f72a2932ce1ae18165e6eee453c9305
bartosz.lagoda <composit095@gmail.com>
2022-06-10 09:27:03 +0200

    Commit LoopsCheck.java

---
+++ src/LoopsCheck.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LoopsCheck {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Masz do wyboru dwa zadania:\n" +
+                "1. Wypisuje elementy z zakresu 1-100 podzielne przez 3\n" +
+                "2. Odwraca elementy tablicy [1,3,5] -> [5,3,1]");
+        int task = scanner.nextInt();
+
+        switch(task){
+            case 1:
+                for(int i = 0; i < 100; i++){
+                    if (i==0){
+                        continue;
+                    }else if(i%3==0){
+                        System.out.println("Liczba : " + i + " jest podzielna przez 3");
+                    }
+                }
+                break;
+            case 2:
+                int[] tablica = new int[]{1,2,3,4,5,6,7,8,9};
+                for(int i=0;i<tablica.length/2;i++){
+                    int temp = tablica[i]; // dla i=0 będzie 1, dla i=1 będzie 3
+                    tablica[i] = tablica[tablica.length-1-i]; //  4 -> 0 3 -> 7
+                    tablica[tablica.length-1-i] = temp; //1 // numbers[3] = 3
+                }
+                for(int i = 0; i < tablica.length; i++){
+                    System.out.println(tablica[i]);
+                }
+        }
+    }
+}
