3f9d9c3a5de184a97e76b1b71ab2691889cb7bd6
bartosz.lagoda <composit095@gmail.com>
2022-06-12 12:12:09 +0200

    Update WebDriverChecker

--- src/Webdriver/WebDriverChecker.java
+++ src/Webdriver/WebDriverChecker.java
@@ -2,13 +2,22 @@ package Webdriver;
 
 public class WebDriverChecker {
     public static void main(String[] args) {
-        ChromeDriver chromeDriver = new ChromeDriver();
-        FirefoxDriver firefoxDriver = new FirefoxDriver();
+        WebDriver chromeDriver = getDriver("chrome");
+        WebDriver firefoxDriver = getDriver("firefox");
 
         System.out.println(firefoxDriver.get("Mozilla Firefox"));
         System.out.println(firefoxDriver.findElementBy("NAME", "BUTTON_1"));
 
         System.out.println(chromeDriver.get("Google Chrome"));
         System.out.println(chromeDriver.findElementBy("XPATH", "//@class['browser']"));
+    }
+
+    private static WebDriver getDriver(String browser) {
+        if (browser.equals("chrome")) {
+            return new ChromeDriver();
+        } else if(browser.equals("firefox")) {
+            return new FirefoxDriver();
+        }
+        return null;
     }
 }
